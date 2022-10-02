29aba94d6f468733e51f408d2e7ead40e57a8ad0
Md Hasibul Hasan <hhasibul677@gmail.com>
2022-10-02 14:15:51 +0600

    first commit

---
+++ pages/InstructorProfileCreate/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorProfileCreate = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default InstructorProfileCreate;
